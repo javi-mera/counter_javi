ca03aec71530d4a5dfff89abd400fff1173c1df2
javi-mera <fcojmera@gmail.com>
2020-11-13 18:50:17 +0000

    ejercicio reformulado utilizando función en vez de clase y props

--- src/js/component/contador.js
+++ src/js/component/contador.js
@@ -1,32 +1,49 @@
 import React from "react";
-//import { render } from "react-dom";
+import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
 import "../../styles/index.scss";
 
-export class Contador extends React.Component {
-	constructor() {
-		super();
-		this.state = {
-			contador: 1
-		};
-
-		setInterval(
-			() => this.setState({ contador: this.state.contador + 1 }),
-			1000
-		);
-	}
-
-	render() {
-		return (
-			<div className="container">
+export function Contador(props) {
+	return (
+		<div>
+			<div className="contador">
 				<p>CONTADOR:</p>
-				<div className="row">
-					<div className="col">
-						<p>{this.state.contador}</p>
-					</div>
-				</div>
+				<div>{props.fifth}</div>
+				<div>{props.fourth}</div>
+				<div>{props.third}</div>
+				<div>{props.second}</div>
+				<div>{props.first}</div>
 			</div>
-		);
-	}
+		</div>
+	);
 }
 
-//render(<SecondsCounter />, document.getElementById("app"));
+Contador.propTypes = {
+	first: PropTypes.number,
+	second: PropTypes.number,
+	third: PropTypes.number,
+	fourth: PropTypes.number,
+	fifth: PropTypes.number
+};
+
+let counter = 0;
+
+setInterval(function() {
+	const one = Math.floor(counter) % 10;
+	const two = Math.floor(counter / 10) % 10;
+	const three = Math.floor(counter / 100) % 10;
+	const four = Math.floor(counter / 1000) % 10;
+	const five = Math.floor(counter / 10000) % 10;
+	counter++;
+
+	ReactDOM.render(
+		<Contador
+			first={one}
+			second={two}
+			third={three}
+			fourth={four}
+			fifth={five}
+		/>,
+		document.querySelector("#app")
+	);
+}, 1000);
